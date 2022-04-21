b7c1d3e0b7217cad96b35bf00fdfa9798c8f965b
Andrey Petrov <nanomaterials@mail.ru>
2022-04-21 21:29:53 +0300

    work with database

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+    image = models.TextField()
+    release_date = models.TextField()
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.price}: {self.image}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -6,12 +7,20 @@ def index(request):
 
 
 def show_catalog(request):
+    product = Phone.objects.all()
+    sort_by = request.GET.get('sort')
+    if sort_by == 'name':
+        product = Phone.objects.all().order_by('name')
+    elif sort_by == 'min_price':
+        product = Phone.objects.all().order_by('price')
+    elif sort_by == 'max_price':
+        product = Phone.objects.all().order_by('-price')
     template = 'catalog.html'
-    context = {}
+    context = {'phones': product}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.get(slug=slug)}
     return render(request, template, context)
