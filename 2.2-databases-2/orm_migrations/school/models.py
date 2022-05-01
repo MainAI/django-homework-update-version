df09950c0146e153a10c866c43ca17571b0ab200
Andrey Petrov <nanomaterials@mail.ru>
2022-05-01 22:20:27 +0300

    orm_migrate

--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Teacher(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
 
@@ -14,8 +15,9 @@ class Teacher(models.Model):
 
 
 class Student(models.Model):
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teacher = models.ManyToManyField(Teacher, related_name='students')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
@@ -23,4 +25,4 @@ class Student(models.Model):
         verbose_name_plural = 'Ученики'
 
     def __str__(self):
-        return self.name
+        return f'{self.name}, {self.id}, {self.teacher}, {self.group}'
