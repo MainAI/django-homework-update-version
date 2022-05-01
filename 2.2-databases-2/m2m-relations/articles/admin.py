7a5cb6adf377b1dc5e71a4ad2d8000ff5f3821d6
Andrey Petrov <nanomaterials@mail.ru>
2022-05-01 22:16:53 +0300

    m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
+from .models import Article, Tag, ArticleTag
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if not form.cleaned_data or form.cleaned_data.get('DELETE') is True:
+                continue
+            if form.cleaned_data.get('is_main', False):
+                count += 1
+        if count > 1:
+            raise ValidationError('Слишком много главных тегов')
+        if count == 0:
+            raise ValidationError('Добавьте главный тег')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Tag.article.through
+    formset = ScopeInlineFormset
+    extra = 5
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'text', 'published_at', 'image']
+    inlines = [
+        ScopeInline,
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
@@ -14,3 +13,29 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, unique=True, verbose_name='Имя тега')
+    article = models.ManyToManyField(Article, through='ArticleTag', related_name='article')
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleTag(models.Model):
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['article', 'tag'], name='some_unique_name')]
+        ordering = ['-is_main', '-tag']
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0}_{1}'.format(self.article, self.tag)
+
+
+
+
+#
